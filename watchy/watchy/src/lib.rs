8291c54a262345da1972c2bc45617de8e0c1a4d1
nmio <kristo.koert@gmail.com>
2020-08-11 13:12:09 +0000

    Progress

--- watchy/watchy/src/lib.rs
+++ watchy/watchy/src/lib.rs
@@ -1,3 +1,41 @@
-pub fn foo() -> bool {
-    true
+use anyhow::Result;
+use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
+
+pub struct Watcher {
+    cmd_to_monitor: Command,
+    cmd_to_trigger: Command,
+
+    running:  bool,
+    interval: Duration,
+}
+
+impl Watcher {
+    pub fn new(cmd_to_monitor: Command, cmd_to_trigger: Command) -> Watcher {
+        Watcher {
+            cmd_to_monitor,
+            cmd_to_trigger,
+
+            running: false,
+            interval: Duration::new(0, 0),
+        }
+    }x
+
+    pub fn watch(&mut self) -> Result<()> {
+        self.running = true;
+        
+        let mut previous_output = Vec::new();
+        while self.running {
+            let out = self.cmd_to_monitor.output()?;
+            if out.stdout != previous_output {
+                self.cmd_to_trigger.spawn()?;
+            }
+            previous_output = out.stdout;
+
+            sleep(self.interval);
+        }
+
+        Ok(())
+    }
 }
