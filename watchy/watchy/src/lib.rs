d2fa84183a3bc01777bca046b6c58977d2830469
Kristo Koert <kristo.koert@gmail.com>
2020-08-13 18:57:15 +0000

    Progress

--- watchy/watchy/src/lib.rs
+++ watchy/watchy/src/lib.rs
@@ -1,14 +1,14 @@
-use anyhow::Result;
+use parking_lot::{Mutex, MutexGuard};
 use std::process::Command;
-use std::thread::sleep;
+use std::sync::Arc;
+use std::thread::{sleep, spawn, JoinHandle};
 use std::time::Duration;
 
 pub struct Watcher {
     cmd_to_monitor: Command,
     cmd_to_trigger: Command,
-
-    running:  bool,
-    interval: Duration,
+    interval:       Duration,
+    is_running:     bool,
 }
 
 impl Watcher {
@@ -17,25 +17,85 @@ impl Watcher {
             cmd_to_monitor,
             cmd_to_trigger,
 
-            running: false,
-            interval: Duration::new(0, 0),
+            is_running: false,
+            interval: Duration::new(1, 0),
         }
-    }x
+    }
 
-    pub fn watch(&mut self) -> Result<()> {
-        self.running = true;
-        
-        let mut previous_output = Vec::new();
-        while self.running {
-            let out = self.cmd_to_monitor.output()?;
-            if out.stdout != previous_output {
-                self.cmd_to_trigger.spawn()?;
+    pub fn interval(&mut self, interval: Duration) -> &mut Watcher {
+        self.interval = interval;
+        self
+    }
+
+    pub fn watch(mut self) -> (Arc<Mutex<Watcher>>, JoinHandle<()>) {
+        self.is_running = true;
+
+        let am = Arc::new(Mutex::new(self));
+        let am_clone = Arc::clone(&am);
+
+        let handle = spawn(move || {
+            let mut previous_output = Vec::new();
+            loop {
+                let mut mg = am.lock();
+                if !mg.is_running {
+                    break;
+                }
+
+                let out = mg.cmd_to_monitor.output().unwrap();
+                if out.stdout != previous_output {
+                    mg.cmd_to_trigger.spawn().unwrap();
+                }
+                previous_output = out.stdout;
+
+                sleep(mg.interval);
+                MutexGuard::unlock_fair(mg);
             }
-            previous_output = out.stdout;
+        });
 
-            sleep(self.interval);
-        }
+        (am_clone, handle)
+    }
 
-        Ok(())
+    pub fn stop(&mut self) {
+        self.is_running = false;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread::sleep;
+
+    #[tokio::test(core_threads = 2)]
+    async fn test_actual_watcher() {
+        let trigger_created_file_name = "trigger_created_file";
+
+        let mut sec_since_epoch = Command::new("date");
+        sec_since_epoch.arg("+%s");
+
+        let mut echo_done = Command::new("touch");
+        echo_done.arg(trigger_created_file_name);
+
+        let interval = Duration::from_millis(250);
+
+        let mut watcher = Watcher::new(sec_since_epoch, echo_done);
+        watcher.interval(interval);
+
+        let (amw, handle) = watcher.watch();
+        sleep(Duration::from_secs(1));
+        amw.lock().stop();
+        handle.join().expect("Join on watcher thread failed");
+
+        let status_code = Command::new("rm")
+            .arg(trigger_created_file_name)
+            .output()
+            .expect("Check of trigger created file failed to run")
+            .status
+            .code()
+            .unwrap();
+
+        assert_eq!(
+            status_code, 0,
+            "Watcher trigger command didn't run or didn't create a file successfully"
+        );
     }
 }
